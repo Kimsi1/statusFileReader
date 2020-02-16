dc0fb5bee1aef234a4de21ec2718bd703ef718b0
Kimmo Niemi <ktniem@utu.fi>
2020-02-16 16:40:20 +0200

    trying functionalities

--- App.js
+++ App.js
@@ -92,7 +92,7 @@ let nameAndDependsArray = [];
 for(i=0;i<dataArray.length;i++){
     var name=getPackageName(dataArray[i]);
     var depends=getDepends(dataArray[i]);
-    nameAndDependsArray[i]=name,depends;
+    nameAndDependsArray[i]={name,depends};
 }
 
 console.log(nameAndDependsArray);
